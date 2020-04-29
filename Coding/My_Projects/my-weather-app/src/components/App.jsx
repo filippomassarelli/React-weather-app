cc1effde5db69e23f54ea0c6901e27aa162e854c
Filippo Massarelli <filippo.massarelli@gmail.com>
2020-04-29 07:39:59 +0100

    API call moved up to parent component. Was stuck and discovered that the wrong callback in useEffect caused infinite loop of fetch calls thus OpenWeather temporarily suspended my account

--- Coding/My_Projects/my-weather-app/src/components/App.jsx
+++ Coding/My_Projects/my-weather-app/src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import Header from "./Header.jsx";
 import Footer from "./Footer";
 import WeatherCard from "./WeatherCard"
@@ -6,21 +6,60 @@ import Search from "./Search"
 
 function App() {
 
-    const [requestedCity, setRequestedCity] = useState("London");
+    const [searchedCity, setSearchedCity] = useState("London")
+
+    const [weather, setWeather] = useState({
+        city: "",
+        description: "",
+        temperature: "",
+        icon: ""
+    });
 
     function searchCity(passedCity){
-        setRequestedCity(passedCity)
+        setSearchedCity(passedCity);
 
+        // setSearchedCity( prevValue => {
+        //     return {
+        //         ...prevValue,
+        //         city: passedCity
+        //     } 
+        // });        
     }
 
-    console.log(requestedCity);
+    const units = "metric";
+    const apiKey = process.env.REACT_APP_API_KEY;
+    let url = "https://api.openweathermap.org/data/2.5/weather?q="+searchedCity+"&appid="+apiKey+"&units="+units;
+
+
+
+    useEffect( () => {
+        async function fetchWeatherData() {
+
+            console.log('useEffect has been called!');
+
+            const response = await fetch(url);
+            const weatherData = await response.json();  
+            
+            console.log(weatherData);
+            
+        
+            setWeather({   
+                city: searchedCity,   
+                description : weatherData.weather[0].description,
+                temperature : Math.round(weatherData.main.temp),
+                icon : "http://openweathermap.org/img/wn/"+weatherData.weather[0].icon+"@2x.png"
+            });
     
+        }
+        fetchWeatherData();
+     }, [searchedCity])
+
 
     return (
         <div>
             <Header />
             <Search onSearch={searchCity} />
-            <WeatherCard city={requestedCity} />
+            <WeatherCard data={weather} />
             <Footer />
         </div>
     );
--- Coding/My_Projects/my-weather-app/src/components/WeatherCard.jsx
+++ Coding/My_Projects/my-weather-app/src/components/WeatherCard.jsx
@@ -1,52 +1,18 @@
-import React, {useState, useEffect} from "react";
-import logo from "../images/02d.png";
+import React from "react";
+
 
 
 
 function WeatherCard(props){
 
-    const [weather, setWeather] = useState({
-        city: "",
-        description: "",
-        temperature: "",
-        iconCode: ""
-    });
 
-    
-    const query = props.city;
-    const units = "metric";
-    const apiKey = process.env.REACT_APP_API_KEY;
-
-    const url = "https://api.openweathermap.org/data/2.5/weather?q="+query+"&appid="+apiKey+"&units="+units;
-
-
-
-useEffect( () => {
-    async function fetchWeatherData() {
-        const response = await fetch(url);
-        const weatherData = await response.json();   
-        
-        setWeather({   
-            city: weatherData.city,   
-            description : weatherData.weather[0].description,
-            temperature : Math.round(weatherData.main.temp),
-            iconCode : weatherData.weather[0].icon
-      });
-    
-    }
-    fetchWeatherData();
-  }, [props.city])
-
-
-  
-  let iconURL = "http://openweathermap.org/img/wn/"+weather.iconCode+"@2x.png";  
 
   return (
       <div className="card">
-          <h1>{props.city}</h1>
-          <img src= {iconURL} />
-          <p>{weather.temperature}°C</p>
-          <p>Currently: {weather.description}</p>
+          <h1>{props.data.city}</h1>
+          <img src= {props.data.icon} />
+          <p>{props.data.temperature}°C</p>
+          <p>Currently: {props.data.description}</p>
       </div>
   )
 
